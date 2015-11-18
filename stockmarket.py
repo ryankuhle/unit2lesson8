237fc121d0c82f410f0a5802b4426096fc1c593a
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-17 18:52:46 -0600

    Notes

--- stockmarket.py
+++ stockmarket.py
@@ -1,8 +1,23 @@
 import pandas as pd
-df = pd.DataFrame({'Bull Market': [.9, .075, .025],
-                   'Bear Market': [.15, .8, .05],
-                   'Stagnant Market': [.25, .25, .50]
+
+#Which model is correct?
+#Conflicting info between actual data frame results and the links below:
+##http://stackoverflow.com/questions/33748031/multiple-transitions-of-markov-model (ignore the code in my initial post)
+##https://en.wikipedia.org/wiki/Markov_chain#Example
+#The  guy on stack overflow gave the same info as the Wikipedia example, which follows a different column/row set up then the Thinkful rainy/sunny comparison
+
+#Model 1
+df = pd.DataFrame({'Bull Market': [.9, .015, .25],
+                   'Bear Market': [.075, .8, .25],
+                   'Stagnant Market': [.025, .05, .50]
                   },
+                  index=["Bull Market", "Bear Market", "Stagnant Market"])
+
+#Model 2
+altdf = pd.DataFrame({'Bull Market': [.9, .075, .025],
+                      'Bear Market': [.15, .8, .05],
+                      'Stagnant Market': [.25, .25, .50]
+                     },
                   index=["Bull Market", "Bear Market", "Stagnant Market"])
 
 print "After one transition of Markov's model"
@@ -11,8 +26,16 @@ print "\n"
 print "After two transitions of Markov's model"
 print(df.dot(df))
 
+#Create function asking for prompt of number of transitions, process
+
+#What are the steady-state probabilities?
+
 '''
-Need to be able to do this X times
-What are the transition probabilities after 2 transitions? After 5? After 10? What are the steady state probabilities (try raising the matrix to a higher and numbers until the probabilities converge)?
-Can you a name some real-life examples that could be modeled by Markov chains? Can you name examples that cannot be treated as Markov chains? Can you name an example of finite probabilistic states that cannot be modeled as Markov chains?
+#Real life examples
+Pepsi wants to know what percentage of cola drinkers will drink Pepsi each year over course of time
+Dice games
+
+
 '''
+
+#Finite probabilistic states not modeled as Markof chains
